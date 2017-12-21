515948b09400aa9393a89f48cb3b4650ae36fdf2
exmaise28 <xmaise28@gmail.com>
2017-12-20 18:52:40 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private items:Item[];
+  private itemInput:string;
+
+  private color:string;
+  private colors:string[];
+
+  constructor(){
+    this.items = [];
+    this.items = [new Item('Milk')]; 
+    this.items.push(new Item('Butter'));
+    this.colors = ["blue", "green", "purple", "cyan"];
+  
+  }
+
+  addItem(){
+    this.items.push(new Item(this.itemInput));
+    this.itemInput = "";
+  }
+
+  removeItem(item:Item){
+ 
+    let index = this.items.findIndex ( a => {
+      return a.name === item.name;})
+    this.items.splice(index,1);
+     }
+
+   colorOnChange(item:Item){
+
+     let color = this.colors[this.random()];
+     item.color = color;
+     }
+
+ random():number{
+     return Math.floor(Math.random() * 3);
+  }
+}
+
+class Item{
+  name:string;
+  color:string;
+  constructor(name:string){
+    this.name = name;
+  }
 }
